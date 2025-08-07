e86bb0ec672abd5db276505a6884d0feeca21384
Duan <daniel.w2301@gmail.com>
2025-08-07 15:05:14 -0500

    add background music

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,8 +7,24 @@ const guessTimes = document.querySelector('.guess-times');
 const guessButton = document.querySelector('.guess');
 const answer = document.querySelector('.answer');
 const flipCard = document.querySelector('.flip-card');
+const clickSound = new Audio('assets/audio/click.mp3');
+const gameSound = new Audio('assets/audio/game.mp3');
+const successSound = new Audio('assets/audio/success.mp3');
+const failSound = new Audio('assets/audio/fail.mp3');
+ 
 let randomNumber;
 let times = 10;
+
+function playGameSound() {
+    gameSound.currentTime = 0;
+    gameSound.play();
+    gameSound.loop = true;
+}
+
+function stopGameSound() {
+    gameSound.pause();
+    gameSound.currentTime = 0;
+}
 
 function generateRandomNumber() {
     randomNumber = Math.floor(Math.random() * 51);
@@ -36,6 +52,7 @@ function initializeGame() {
     guessButton.style.cursor = 'pointer';
     flipCard.style.transform = 'rotateY(0deg)';
     output.value = 'Enter a number in the circle';
+
 }
 
 function endGame() {
@@ -44,9 +61,11 @@ function endGame() {
     input.style.cursor = 'not-allowed';
     guessButton.style.cursor = 'not-allowed';
     playButton.style.cursor = 'pointer';
+    stopGameSound();
 }
 
 function checkNumber(isLastGuess = false) {
+    clickSound.play();
     const userGuess = parseInt(input.value.trim(), 10);
 
     if (!validateInput(input.value)) return; 
@@ -55,11 +74,22 @@ function checkNumber(isLastGuess = false) {
     guessTimes.innerText = `Guesses Left: ${times}`;
 
     if (userGuess > randomNumber) {
-        output.value = isLastGuess ? 'Game Over' : 'My number is smaller';
+        if (isLastGuess) {
+            output.value = 'Game Over';
+            failSound.play();
+        } else {
+            output.value = 'My number is smaller';
+        }
     } else if (userGuess < randomNumber) {
-        output.value = isLastGuess ? 'Game Over' : 'My number is bigger';
+        if (isLastGuess) {
+            output.value = 'Game Over';
+            failSound.play();
+        } else {
+            output.value = 'My number is bigger';
+        }
     } else {
         output.value = 'Correct Guess!!!';
+        successSound.play();
         answer.innerText = `${randomNumber}`;
         flipCard.style.transform = 'rotateY(180deg)';
         endGame(); 
@@ -80,6 +110,8 @@ guessButton.addEventListener('click', function() {
 });
 
 playButton.addEventListener('click', () => {
+    clickSound.play();
+    playGameSound();
     initializeGame();
     input.focus();
     playButton.innerText = 'RESTART';
